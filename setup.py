07f6efac54920fa0650ce4dbee6af205a5054ab6
Prem <kumarprem10694@gmail.com>
2025-03-26 19:59:04 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    
+    name="GEN AI PROJECT",
+    version='0.0.0',
+    author='GOD PREM',
+    packages = find_packages(),
+    install_requires = []
+)
